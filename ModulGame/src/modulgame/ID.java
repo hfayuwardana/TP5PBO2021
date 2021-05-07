d5cf5af6d91e4aa8ccd9d1f5d00adf760eace131
hfayuwardana <hikmawatifajriahayuwardana@gmail.com>
2021-05-07 15:20:10 +0700

    Menambahkan ModulGame

---
+++ ModulGame/src/modulgame/ID.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modulgame;
+
+/**
+ *
+ * @author Fauzan
+ */
+public enum ID {
+    Player(), // id untuk player 1
+    Player2(), // id untuk player 2
+    Enemy(), // id untuk enemy
+    Item(); // id untuk item
+}
